3a61c826690acb7bb5cdbe23e55da8bbf9df5208
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-03-23 11:35:56 +0300

    Update Slicing_in_Python.py

--- Slicing_in_Python.py
+++ Slicing_in_Python.py
@@ -1,3 +1,15 @@
+# File: Slicing_in_Python.py
+# Description: How to slice strings in Python
+# Environment: PyCharm and Anaconda environment
+#
+# MIT License
+# Copyright (c) 2018 Valentyn N Sichkar
+# github.com/sichkar-valentyn
+#
+# Reference to:
+# [1] Valentyn N Sichkar. Slicing in Python // GitHub platform [Electronic resource]. URL: https://github.com/sichkar-valentyn/Slicing_in_Python (date of access: XX.XX.XXXX)
+
+
 # Initial string
 dna = 'ATTCGGAGCT'
 #   = '0123456789'
